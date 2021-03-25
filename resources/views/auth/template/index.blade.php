2c297bbefc920858fc3392259bb17c8368933906
Luiz Felipe Pereira <felipepwned@hotmail.com>
2021-03-24 23:55:45 -0300

    Rotas e Tela de Login

---
+++ resources/views/auth/template/index.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+@includeIf("auth.template.head")
+
+<body class="hold-transition login-page">
+     @yield("content")
+     @includeIf("auth.template.javascript")
+
+</body>
+
+</html>
